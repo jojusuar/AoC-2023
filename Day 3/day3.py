0ad0ddd47810876535560605a1a83270a05fde3d
jojusuar <jojusuar@espol.edu.ec>
2024-09-14 13:48:24 -0500

    day 3 part 2

--- Day 3/day3.py
+++ Day 3/day3.py
@@ -1,8 +1,12 @@
+from functools import reduce
+
 archive = open('input.txt', 'r')
 lines = list(archive.readlines())
 archive.close()
 
 part1 = 0
+part2 = 0
+adjacencyTable = {} 
 
 def isAdjacent(number):
     for x in number['columns']:
@@ -11,6 +15,18 @@ def isAdjacent(number):
             if(not (symbol.isdigit() or symbol == '.' or symbol == '\n')):
                 return True
     return False
+
+def adjacencies(number):
+    for x in number['columns']:
+        for y in number['rows']:
+            symbol = lines[y][x]
+            if(not (symbol.isdigit() or symbol == '.' or symbol == '\n')):
+                if(symbol == '*'):
+                    numbers = adjacencyTable.get((x, y))
+                    if(numbers is None):
+                        adjacencyTable[(x, y)] = [int(number['number'])]
+                    else:
+                        adjacencyTable[(x, y)].append(int(number['number']))
                 
 for i in range(len(lines)):
     row = lines[i]
@@ -32,19 +48,22 @@ for i in range(len(lines)):
             
             if 0 < number['columns'][0]:
                 number['columns'].insert(0, number['columns'][0] - 1)
-                number['left'] = False
             if number['columns'][-1] < len(row) - 1:
                 number['columns'].append(number['columns'][-1] + 1)
-                number['right'] = False
             if 0 < number['rows'][0]:
                 number['rows'].insert(0, number['rows'][0] - 1)
-                number['top'] = False
             if number['rows'][-1] < len(lines) -1:
                 number['rows'].append(number['rows'][-1] + 1)
-                number['bottom'] = False
             
             if(isAdjacent(number)):
                 part1 += int(number['number'])
+            adjacencies(number)
         j += 1
         
 print(part1)
+
+for numberList in adjacencyTable.values():
+    if(len(numberList) == 2):
+        part2 += reduce(lambda x, y: x*y, numberList)
+
+print(part2)
